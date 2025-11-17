877d0b5ce2763ca21e322f1b6b952fe9a6634a35
Maksim <sharkomaks@gmail.com>
2025-11-17 14:35:43 +0300

    Refactor index.ts to streamline exports and include error handling utilities

--- src/shared/api/index.ts
+++ src/shared/api/index.ts
@@ -1,4 +1,1 @@
 export { baseApi } from './api';
-export { specializationsApi, useGetSpecializationsQuery } from './specializations.api';
-export { skillsApi, useGetSkillsQuery } from './skills.api';
-export { quizApi, useGetNewMockQuizQuery } from './quiz.api';
--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -1,11 +1,4 @@
 // Common types
 export type { PaginatedResponse } from './api';
 
-// Specializations
-export type { Specialization, SpecializationsResponse } from './specializations.types';
-
-// Skills
-export type { Skill, SkillsResponse } from './skills.types';
-
-// Quiz
-export type { Question, QuizAnswer, QuizAnswersInfo, QuizAuthor, QuizParams, QuizResponse } from './quiz.types';
+export { ApiErrorType, getErrorMessage } from './errors';
